66a6b350c99ad07abf8ca3a1fec910f292784f2c
aswinblue <jepilon@naver.com>
2022-02-05 15:25:31 +0900

    2022-02-05 15:25:31.53

--- upload.bat
+++ upload.bat
@@ -1,6 +1,14 @@
-git add content/*
-git commit -m "%date% %time%"
+::build project
 hugo -t hugo-notepadium
+
+::add & commit public
 cd public
 git add *
 git commit -m "%date% %time%"
+git push origin master
+
+::add & commit source
+cd ..
+git add content/*
+git commit -m "%date% %time%"
+git push origin master
