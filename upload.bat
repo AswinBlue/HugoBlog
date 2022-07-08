c103abaf098343e201a93001db80115b6e56f84d
AswinBlue <aswindblew@gmail.com>
2022-07-08 20:47:32 +0900

    (Add) new theme PaperMod

--- upload.bat
+++ upload.bat
@@ -1,14 +1,15 @@
 ::build project
-hugo -t hugo-notepadium
+::hugo -t hugo-notepadium
+hugo -t hugo-PaperMod
 
 ::add & commit public
 cd public
 git add *
 git commit -m "%date% %time%"
 git push origin master
+cd ..
 
 ::add & commit source
-cd ..
 git add content/*
 git commit -m "%date% %time%"
 git push origin master
